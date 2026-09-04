4de6b1afcb0fa3fdf0b80e7a8d95569834d064e3
RaelMartinss <raelpgm2@gmail.com>
2026-09-04 14:47:09 -0300

    fix(main): enable global ValidationPipe to validate DTOs and return 400 Bad Request on invalid payload

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { DomainExceptionFilter } from './modules/drivers/infrastructure/http/domain-exception.filter';
-import { VersioningType } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +23,14 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
 
   const config = new DocumentBuilder()
   .setTitle('Portal Frotas API')
